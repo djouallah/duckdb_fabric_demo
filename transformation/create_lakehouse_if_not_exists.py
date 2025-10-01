df6e90a4bb90a7c13a30592dab5f1da3c2c494b4
Mimoune <djouallah@users.noreply.github.com>
2025-10-01 15:00:01 +1000

    Update create_lakehouse_if_not_exists.py

--- transformation/create_lakehouse_if_not_exists.py
+++ transformation/create_lakehouse_if_not_exists.py
@@ -1,11 +1,12 @@
 import notebookutils
+import sempy.fabric as fabric
 def create_lakehouse_if_not_exists(lakehouse_name):
     try:
         notebookutils.lakehouse.get(lakehouse_name)
         return 1
     except:
         try:
-            notebookutils.lakehouse.create(lakehouse_name)
+            fabric.create_lakehouse(lakehouse_name,enable_schema=True)
             notebookutils.lakehouse.get(lakehouse_name)
             return 1
         except:
