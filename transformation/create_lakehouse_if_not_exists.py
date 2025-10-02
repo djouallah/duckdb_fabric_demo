865af48adfc312e264a1a5d6d265543b8f4c1693
Mimoune <djouallah@users.noreply.github.com>
2025-10-02 13:06:20 +1000

    use notebookutilis with schema_enabled on

--- transformation/create_lakehouse_if_not_exists.py
+++ transformation/create_lakehouse_if_not_exists.py
@@ -1,12 +1,11 @@
 import notebookutils
-import sempy.fabric as fabric
 def create_lakehouse_if_not_exists(lakehouse_name):
     try:
         notebookutils.lakehouse.get(lakehouse_name)
         return 1
     except:
         try:
-            fabric.create_lakehouse(lakehouse_name,enable_schema=True)
+            notebookutils.lakehouse.create(lakehouse_name,definition={"enableSchemas": True})
             notebookutils.lakehouse.get(lakehouse_name)
             return 1
         except:
